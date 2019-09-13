339a57ede9a61b272b3cad152548d9d9dc6520e1
FearTheDev <w.m.t.b5115@gmail.com>
2019-09-12 23:47:03 -0400

    Updated PokeAPI to work with sprites

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 const PokeCard = props => {
-  if (!props) {
-    return <div>Loading...</div>
+
+  const {data} = props;
+
+
+  
+  if (!data) {
+    return <h2>Loading..</h2>;
   }
 
-  const { front_default } = props.pokeData.sprites;
-
+  console.log(data);
   return ( 
     <div>
-      <h3>{front_default}</h3>
-      <h3>{props.pokeData.name}</h3>
-      <h3>{props.pokeData.weight}</h3>
+      <img src={data.sprites.front_default} alt={data.name} />
+      <h3>{data.name}</h3>
+      <h3>{data.weight}</h3>
+      <h3>{data.id}</h3>
     </div>
-  )
+  );
+
 }
 
 export default PokeCard;
--- src/components/PokeData.js
+++ src/components/PokeData.js
@@ -3,19 +3,36 @@ import PokeCard from './PokeCard';
 import axios from 'axios';
 
 const PokeData = () => {
-  const [pokeData, setPokeData] = useState({});
+  const [pokeData, setPokeData] = useState();
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    axios.get('https://pokeapi.co/api/v2/pokemon/4')
       .then(res => {
-        setPokeData(res.data)
+        let data = {};
+        data.abilities = res.data.abilities;
+        data.base_experience = res.data.base_experience;
+        data.forms = res.data.forms;
+        data.game_indices = res.data.game_indices;
+        data.height = res.data.height;
+        data.held_items = res.data.held_items;
+        data.id = res.data.id;
+        data.is_default = res.data.is_default;
+        data.location_area_encounters = res.data.location_area_encounters;
+        data.moves = res.data.moves;
+        data.name = res.data.name;
+        data.species = res.data.species;
+        data.sprites = res.data.sprites;
+        data.stats = res.data.stats;
+        data.types = res.data.types;
+        data.weight = res.data.weight;
+        setPokeData(data);
       })
       .catch(err => console.log('Error', err))
   }, []);
 
   return (
     <div>
-      <PokeCard pokeData={pokeData}/>
+      <PokeCard data={pokeData}/>
     </div>
   )
 }
