fc760e18aa944161271db79569dc8f057419ae42
FearTheDev <w.m.t.b5115@gmail.com>
2019-09-14 09:03:01 -0400

    Type Badge Component + Less style updated

--- src/components/PokeTypeBadge.jsx
+++ src/components/PokeTypeBadge.jsx
@@ -5,7 +5,7 @@ const PokeTypeBadge = props =>{
     if(!props.type){
 
     }
-    return (<p className={'badge-type-'+ props.type}>{props.type.toUpperCase()}</p>);
+    return (<p className={'badge-type-'+ props.type.toString()}>{props.type.toUpperCase()}</p>);
 };
 
 export default PokeTypeBadge;
