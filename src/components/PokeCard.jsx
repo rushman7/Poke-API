6967bb79f00f5951ec25f549795ee90a0134e4be
FearTheDev <w.m.t.b5115@gmail.com>
2019-09-14 09:00:19 -0400

    Minor corrections + type badge added

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -13,7 +13,7 @@ const PokeCard = props => {
               {pokeData.name.charAt(0).toUpperCase() +
                 pokeData.name.substring(1)}
             </h2>
-            <i class="gender fa fa-venus" aria-hidden="true"></i>
+            <i className="gender fa fa-venus" aria-hidden="true"></i>
             <h3>
               Lv. {Math.round(Math.random() * 100)}
             </h3>
@@ -31,7 +31,7 @@ const PokeCard = props => {
         <div className="poke-card-right">
           <div className="poke-card-topright">
             <img src={pokeData.sprites.front_default} alt={pokeData.name} />
-            <h3><i class="fa fa-minus" aria-hidden="true"></i> {(parseInt(pokeData.id) < 10 ? "00" : parseInt(pokeData.id) < 100 ? "0":  "") + pokeData.id } <i class="fa fa-minus" aria-hidden="true"></i></h3>
+            <h3><i className="fa fa-minus" aria-hidden="true"></i> {(parseInt(pokeData.id) < 10 ? "00" : parseInt(pokeData.id) < 100 ? "0":  "") + pokeData.id } <i className="fa fa-minus" aria-hidden="true"></i></h3>
             <div className="poke-type">
               {pokeData.types.map((attr, index) => (
                 <PokeTypeBadge key={index} type={attr.type.name}/>
@@ -42,7 +42,7 @@ const PokeCard = props => {
           <div className="poke-skills">
             <h3>MOVES LEARNED</h3>
             {pokeData.abilities.map((skill, index) => (
-              <p key={index}><i class="poke-card-move-bullet fa fa-caret-right" aria-hidden="true"></i> {skill.ability.name.toUpperCase()}</p>
+              <p key={index}><i className="poke-card-move-bullet fa fa-caret-right" aria-hidden="true"></i> {skill.ability.name.toUpperCase()}</p>
             ))}
           </div>
         </div>
