9d6853566238d6cbe2c73177cb4b3e61764d37b0
BuildTools <arthurpisakhov1@gmail.com>
2019-09-13 20:22:57 -0400

    updated styles

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { white } from 'ansi-colors';
 
 const PokeCard = props => {
+
+  const typeColor = {
+  backgroundColor: '#F98133',
+  color: 'white',
+  border: '2px solid white'
+}
 
   const {pokeData} = props;
 
@@ -9,18 +16,22 @@ const PokeCard = props => {
   }
 
   return ( 
-    <div>
-      <img src={pokeData.sprites.front_default} alt={pokeData.name}/>
-      <p>{pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1)} Lv. {Math.round(Math.random() * 100)}</p>
-      <p>DEX NO. {(parseInt(pokeData.id) < 10 ? '0' : '') + pokeData.id}</p>
-      <div className='poke-skills'>
-        MOVES LEARNED {pokeData.abilities.map(skill => <p key={skill.ability.name}>{skill.ability.name}</p>)}
+    <div className='poke-card'>
+      <div className='poke-card-left'>
+        <p>{pokeData.name.charAt(0).toUpperCase() + pokeData.name.substring(1)} Lv. {Math.round(Math.random() * 100)}</p>
+        <div className='poke-stats'>
+          <p>DEX NO. {(parseInt(pokeData.id) < 10 ? '0' : '') + pokeData.id}</p>
+          {pokeData.stats.map(attr => <p key={attr.stat.name}>{attr.stat.name.toUpperCase()} {attr.base_stat}</p>)}
+        </div>
       </div>
-      <div className='poke-type'>
-        {pokeData.types.map(attr => <p key={attr.type.url}>{attr.type.name}</p>)}
-      </div>
-      <div className='poke-stats'>
-        {pokeData.stats.map(attr => <p key={attr.stat.name}>{attr.stat.name.toUpperCase()} {attr.base_stat}</p>)}
+      <div className='poke-card-right'>
+        <img src={pokeData.sprites.front_default} alt={pokeData.name}/>
+        <div className='poke-type'>
+          {pokeData.types.map(attr => <p style={typeColor} key={attr.type.url}>{attr.type.name.toUpperCase()}</p>)}
+        </div>
+        <div className='poke-skills'>
+          MOVES LEARNED {pokeData.abilities.map(skill => <p key={skill.ability.name} >{skill.ability.name.toUpperCase()}</p>)}
+        </div>
       </div>
     </div>
   )
